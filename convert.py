3a159d9e97780d874dff680401651a17a5d42343
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-06-27 12:20:20 -0700

    Added README and Generated Template

---
+++ convert.py
@@ -0,0 +1,8 @@
+import matplotlib.image as image
+
+
+img=image.imread('stockimage.jpeg')
+print('The Shape of the image is:',img.shape)
+print('The image as array is:')
+#print(img)
+print("The size of the array/matrix is:", img.size)
