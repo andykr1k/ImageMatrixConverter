f38c5daddfd9abd14ac2fce69f3fcb9a4b584c72
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-06-29 00:57:21 -0700

    Finished Pixel Cases - Still Needs to Push to new array

--- Dev/test.py
+++ Dev/test.py
@@ -16,187 +16,409 @@ print("The size of img[0][0][0] is : ", img[0][0][0].size, " The values of img[0
 # img[rows][columns][0 - red // 1 - blue // 2 - green]
 
 #Calculates Averages from AxesImage Matrix and Stores into a new AxesImage Matrix
-
 #Dimensions of Image and Current Pixel
-i = 0
-j =0
+r = 0
+c =0
 rowmax = img.shape[0] - 1
 colmax = img.shape[1] - 1
 
 #If Heirarchy for different pixel cases
-if (i == 0 and j ==0):
-    # Top Left Pixel
-    something = 0
-elif (i == 0 and j == colmax):
-    # Top Right Pixel
-    something = 0
-elif (i == rowmax and j == 0):
-    # Bottom Left Pixel
-    something = 0
-elif (i == rowmax and j == colmax):
-    # Bottom Right Pixel
-    something = 0
-elif (i == 0):
-    # Top Row
-    something = 0
-elif (i == rowmax):
-    # Bottom Row
-    something = 0
-elif (j == 0):
-    # Left Column
-    something = 0
-elif (j == colmax):
-    # Right Column
-    something = 0
-else:
-    # Middle Pixels
-    something = 0
+if (r == 0 and c ==0): # Top Left Pixel
+    #####################Average of quarter layer pixels (2x2)(Top Left)##########################
+    redCounter = 0
+    blueCounter = 0
+    greenCounter = 0
+    middle = 0.44445
+    side = 0.22223
+    diag = 0.11112
 
-#####################Average of full layer pixels (3x3)(Middle Pixels)##########################
-redCounter = 0
-blueCounter = 0
-greenCounter = 0
-middle = 0.25
-side = 0.125
-diag = 0.0625
-r = 1
-c = 1
-#Top left
-redCounter = img[r-1][c-1][0]*diag + redCounter
-blueCounter = img[r-1][c-1][1]*diag + blueCounter
-greenCounter = img[r-1][c-1][2]*diag + greenCounter
+    #Middle
+    redCounter = img[r][c][0]*middle + redCounter
+    blueCounter = img[r][c][1]*middle + blueCounter
+    greenCounter = img[r][c][2]*middle + greenCounter
 
-#Top middle
-redCounter = img[r][c-1][0]*side + redCounter
-blueCounter = img[r][c-1][1]*side + blueCounter
-greenCounter = img[r][c-1][2]*side + greenCounter
+    #Right
+    redCounter = img[r+1][c][0]*side + redCounter
+    blueCounter = img[r+1][c][1]*side + blueCounter
+    greenCounter = img[r+1][c][2]*side + greenCounter
 
-#Top right
-redCounter = img[r+1][c+1][0]*diag + redCounter
-blueCounter = img[r+1][c+1][1]*diag + blueCounter
-greenCounter = img[r+1][c+1][2]*diag + greenCounter
+    #Bottom middle
+    redCounter = img[r][c+1][0]*side + redCounter
+    blueCounter = img[r][c+1][1]*side + blueCounter
+    greenCounter = img[r][c+1][2]*side + greenCounter
 
-#Left
-redCounter = img[r-1][c][0]*side + redCounter
-blueCounter = img[r-1][c][1]*side + blueCounter
-greenCounter = img[r-1][c][2]*side + greenCounter
+    #Bottom right
+    redCounter = img[r+1][c+1][0]*diag + redCounter
+    blueCounter = img[r+1][c+1][1]*diag + blueCounter
+    greenCounter = img[r+1][c+1][2]*diag + greenCounter
 
-#Middle
-redCounter = img[r][c][0]*middle + redCounter
-blueCounter = img[r][c][1]*middle + blueCounter
-greenCounter = img[r][c][2]*middle + greenCounter
+    redAvg = redCounter
+    blueAvg = blueCounter
+    greenAvg = greenCounter
 
-#Right
-redCounter = img[r+1][c][0]*side + redCounter
-blueCounter = img[r+1][c][1]*side + blueCounter
-greenCounter = img[r+1][c][2]*side + greenCounter
+    print("The average of the red pixels is:", redAvg)
+    print("The average of the blue pixels is:", blueAvg)
+    print("The average of the green pixels is:", greenAvg)
+    #####################Average of quarter layer pixels (2x2)##########################
+elif (r == 0 and c == colmax):  # Top Right Pixel
+    #####################Average of quarter layer pixels (2x2)(Top Right)##########################
+    redCounter = 0
+    blueCounter = 0
+    greenCounter = 0
+    middle = 0.44445
+    side = 0.22223
+    diag = 0.11112
 
-#Bottom left
-redCounter = img[r-1][c+1][0]*diag + redCounter
-blueCounter = img[r-1][c+1][1]*diag + blueCounter
-greenCounter = img[r-1][c+1][2]*diag + greenCounter
+    #Middle
+    redCounter = img[r][c][0]*middle + redCounter
+    blueCounter = img[r][c][1]*middle + blueCounter
+    greenCounter = img[r][c][2]*middle + greenCounter
 
-#Bottom middle
-redCounter = img[r][c+1][0]*side + redCounter
-blueCounter = img[r][c+1][1]*side + blueCounter
-greenCounter = img[r][c+1][2]*side + greenCounter
+    #Left
+    redCounter = img[r-1][c][0]*side + redCounter
+    blueCounter = img[r-1][c][1]*side + blueCounter
+    greenCounter = img[r-1][c][2]*side + greenCounter
 
-#Bottom right
-redCounter = img[r+1][c+1][0]*diag + redCounter
-blueCounter = img[r+1][c+1][1]*diag + blueCounter
-greenCounter = img[r+1][c+1][2]*diag + greenCounter
+    #Bottom middle
+    redCounter = img[r][c+1][0]*side + redCounter
+    blueCounter = img[r][c+1][1]*side + blueCounter
+    greenCounter = img[r][c+1][2]*side + greenCounter
 
-redAvg = redCounter
-blueAvg = blueCounter
-greenAvg = greenCounter
-print("The average of the red pixels is:", redAvg)
-print("The average of the blue pixels is:", blueAvg)
-print("The average of the green pixels is:", greenAvg)
-#####################Average of full layer pixels (3x3)##########################
+    #Bottom left
+    redCounter = img[r-1][c+1][0]*diag + redCounter
+    blueCounter = img[r-1][c+1][1]*diag + blueCounter
+    greenCounter = img[r-1][c+1][2]*diag + greenCounter
+
+    redAvg = redCounter
+    blueAvg = blueCounter
+    greenAvg = greenCounter
+
+    print("The average of the red pixels is:", redAvg)
+    print("The average of the blue pixels is:", blueAvg)
+    print("The average of the green pixels is:", greenAvg)
+    #####################Average of quarter layer pixels (2x2)##########################
+elif (r == rowmax and c == 0): # Bottom Left Pixel
+    #####################Average of quarter layer pixels (2x2)(Bottom Left)##########################
+    redCounter = 0
+    blueCounter = 0
+    greenCounter = 0
+    middle = 0.44445
+    side = 0.22223
+    diag = 0.11112
+
+    #Middle
+    redCounter = img[r][c][0]*middle + redCounter
+    blueCounter = img[r][c][1]*middle + blueCounter
+    greenCounter = img[r][c][2]*middle + greenCounter
+
+    #Right
+    redCounter = img[r+1][c][0]*side + redCounter
+    blueCounter = img[r+1][c][1]*side + blueCounter
+    greenCounter = img[r+1][c][2]*side + greenCounter
+
+    #Top middle
+    redCounter = img[r][c-1][0]*side + redCounter
+    blueCounter = img[r][c-1][1]*side + blueCounter
+    greenCounter = img[r][c-1][2]*side + greenCounter
+
+    #Top right
+    redCounter = img[r+1][c-1][0]*diag + redCounter
+    blueCounter = img[r+1][c-1][1]*diag + blueCounter
+    greenCounter = img[r+1][c-1][2]*diag + greenCounter
+
+    redAvg = redCounter
+    blueAvg = blueCounter
+    greenAvg = greenCounter
+
+    print("The average of the red pixels is:", redAvg)
+    print("The average of the blue pixels is:", blueAvg)
+    print("The average of the green pixels is:", greenAvg)
+    #####################Average of quarter layer pixels (2x2)##########################
+elif (r == rowmax and c == colmax): # Bottom Right Pixel
+    #####################Average of quarter layer pixels (2x2)(Bottom Right)##########################
+    redCounter = 0
+    blueCounter = 0
+    greenCounter = 0
+    middle = 0.44445
+    side = 0.22223
+    diag = 0.11112
+
+    #Left
+    redCounter = img[r-1][c][0]*side + redCounter
+    blueCounter = img[r-1][c][1]*side + blueCounter
+    greenCounter = img[r-1][c][2]*side + greenCounter
+
+    #Middle
+    redCounter = img[r][c][0]*middle + redCounter
+    blueCounter = img[r][c][1]*middle + blueCounter
+    greenCounter = img[r][c][2]*middle + greenCounter
+
+    #Top left
+    redCounter = img[r-1][c-1][0]*diag + redCounter
+    blueCounter = img[r-1][c-1][1]*diag + blueCounter
+    greenCounter = img[r-1][c-1][2]*diag + greenCounter
+
+    #Top middle
+    redCounter = img[r][c-1][0]*side + redCounter
+    blueCounter = img[r][c-1][1]*side + blueCounter
+    greenCounter = img[r][c-1][2]*side + greenCounter
+
+    redAvg = redCounter
+    blueAvg = blueCounter
+    greenAvg = greenCounter
+
+    print("The average of the red pixels is:", redAvg)
+    print("The average of the blue pixels is:", blueAvg)
+    print("The average of the green pixels is:", greenAvg)
+    #####################Average of quarter layer pixels (2x2)##########################
+elif (r == 0): # Top Row
+    #####################Average of half layer pixels (2x3)(Top Row)##########################
+    redCounter = 0
+    blueCounter = 0
+    greenCounter = 0
+    middle = 0.33334
+    side = 0.16667
+    diag = 0.08334
+
+    #Left
+    redCounter = img[r-1][c][0]*side + redCounter
+    blueCounter = img[r-1][c][1]*side + blueCounter
+    greenCounter = img[r-1][c][2]*side + greenCounter
+
+    #Middle
+    redCounter = img[r][c][0]*middle + redCounter
+    blueCounter = img[r][c][1]*middle + blueCounter
+    greenCounter = img[r][c][2]*middle + greenCounter
+
+    #Right
+    redCounter = img[r+1][c][0]*side + redCounter
+    blueCounter = img[r+1][c][1]*side + blueCounter
+    greenCounter = img[r+1][c][2]*side + greenCounter
+
+    #Bottom left
+    redCounter = img[r-1][c+1][0]*diag + redCounter
+    blueCounter = img[r-1][c+1][1]*diag + blueCounter
+    greenCounter = img[r-1][c+1][2]*diag + greenCounter
+
+    #Bottom middle
+    redCounter = img[r][c+1][0]*side + redCounter
+    blueCounter = img[r][c+1][1]*side + blueCounter
+    greenCounter = img[r][c+1][2]*side + greenCounter
+
+    #Bottom right
+    redCounter = img[r+1][c+1][0]*diag + redCounter
+    blueCounter = img[r+1][c+1][1]*diag + blueCounter
+    greenCounter = img[r+1][c+1][2]*diag + greenCounter
+
+    redAvg = redCounter
+    blueAvg = blueCounter
+    greenAvg = greenCounter
+
+    print("The average of the red pixels is:", redAvg)
+    print("The average of the blue pixels is:", blueAvg)
+    print("The average of the green pixels is:", greenAvg)
+    #####################Average of half layer pixels (2x3)##########################
+elif (r == rowmax): # Bottom Row
+    #####################Average of half layer pixels (2x3)(Bottom Row)##########################
+    redCounter = 0
+    blueCounter = 0
+    greenCounter = 0
+    middle = 0.33334
+    side = 0.16667
+    diag = 0.08334
+
+    #Left
+    redCounter = img[r-1][c][0]*side + redCounter
+    blueCounter = img[r-1][c][1]*side + blueCounter
+    greenCounter = img[r-1][c][2]*side + greenCounter
+
+    #Middle
+    redCounter = img[r][c][0]*middle + redCounter
+    blueCounter = img[r][c][1]*middle + blueCounter
+    greenCounter = img[r][c][2]*middle + greenCounter
+
+    #Right
+    redCounter = img[r+1][c][0]*side + redCounter
+    blueCounter = img[r+1][c][1]*side + blueCounter
+    greenCounter = img[r+1][c][2]*side + greenCounter
+
+    #Top left
+    redCounter = img[r-1][c-1][0]*diag + redCounter
+    blueCounter = img[r-1][c-1][1]*diag + blueCounter
+    greenCounter = img[r-1][c-1][2]*diag + greenCounter
+
+    #Top middle
+    redCounter = img[r][c-1][0]*side + redCounter
+    blueCounter = img[r][c-1][1]*side + blueCounter
+    greenCounter = img[r][c-1][2]*side + greenCounter
+
+    #Top right
+    redCounter = img[r+1][c+1][0]*diag + redCounter
+    blueCounter = img[r+1][c+1][1]*diag + blueCounter
+    greenCounter = img[r+1][c+1][2]*diag + greenCounter
 
 
-#####################Average of half layer pixels (2x3)(Top Row)##########################
-redCounter = 0
-blueCounter = 0
-greenCounter = 0
-middle = 0.33334
-side = 0.16667
-diag = 0.08334
-r = 0
-c = 1
-#Left
-redCounter = img[r-1][c][0]*side + redCounter
-blueCounter = img[r-1][c][1]*side + blueCounter
-greenCounter = img[r-1][c][2]*side + greenCounter
+    redAvg = redCounter
+    blueAvg = blueCounter
+    greenAvg = greenCounter
 
-#Middle
-redCounter = img[r][c][0]*middle + redCounter
-blueCounter = img[r][c][1]*middle + blueCounter
-greenCounter = img[r][c][2]*middle + greenCounter
+    print("The average of the red pixels is:", redAvg)
+    print("The average of the blue pixels is:", blueAvg)
+    print("The average of the green pixels is:", greenAvg)
+    #####################Average of half layer pixels (2x3)##########################
+elif (c == 0): # Left Column
+    #####################Average of half layer pixels (2x3)(Left Coloumn)##########################
+    redCounter = 0
+    blueCounter = 0
+    greenCounter = 0
+    middle = 0.33334
+    side = 0.16667
+    diag = 0.08334
 
-#Right
-redCounter = img[r+1][c][0]*side + redCounter
-blueCounter = img[r+1][c][1]*side + blueCounter
-greenCounter = img[r+1][c][2]*side + greenCounter
+    #Middle
+    redCounter = img[r][c][0]*middle + redCounter
+    blueCounter = img[r][c][1]*middle + blueCounter
+    greenCounter = img[r][c][2]*middle + greenCounter
 
-#Bottom left
-redCounter = img[r-1][c+1][0]*diag + redCounter
-blueCounter = img[r-1][c+1][1]*diag + blueCounter
-greenCounter = img[r-1][c+1][2]*diag + greenCounter
-
-#Bottom middle
-redCounter = img[r][c+1][0]*side + redCounter
-blueCounter = img[r][c+1][1]*side + blueCounter
-greenCounter = img[r][c+1][2]*side + greenCounter
-
-#Bottom right
-redCounter = img[r+1][c+1][0]*diag + redCounter
-blueCounter = img[r+1][c+1][1]*diag + blueCounter
-greenCounter = img[r+1][c+1][2]*diag + greenCounter
-
-redAvg = redCounter
-blueAvg = blueCounter
-greenAvg = greenCounter
-print("The average of the red pixels is:", redAvg)
-print("The average of the blue pixels is:", blueAvg)
-print("The average of the green pixels is:", greenAvg)
-#####################Average of half layer pixels (2x3)##########################
+    #Right
+    redCounter = img[r+1][c][0]*side + redCounter
+    blueCounter = img[r+1][c][1]*side + blueCounter
+    greenCounter = img[r+1][c][2]*side + greenCounter
 
 
-#####################Average of quarter layer pixels (2x2)(Top Left)##########################
-redCounter = 0
-blueCounter = 0
-greenCounter = 0
-middle = 0.44445
-side = 0.22223
-diag = 0.11112
-r = 0
-c = 0
+    #Top middle
+    redCounter = img[r][c-1][0]*side + redCounter
+    blueCounter = img[r][c-1][1]*side + blueCounter
+    greenCounter = img[r][c-1][2]*side + greenCounter
 
-#Middle
-redCounter = img[r][c][0]*middle + redCounter
-blueCounter = img[r][c][1]*middle + blueCounter
-greenCounter = img[r][c][2]*middle + greenCounter
+    #Top right
+    redCounter = img[r+1][c+1][0]*diag + redCounter
+    blueCounter = img[r+1][c+1][1]*diag + blueCounter
+    greenCounter = img[r+1][c+1][2]*diag + greenCounter
 
-#Right
-redCounter = img[r+1][c][0]*side + redCounter
-blueCounter = img[r+1][c][1]*side + blueCounter
-greenCounter = img[r+1][c][2]*side + greenCounter
+    #Bottom middle
+    redCounter = img[r][c+1][0]*side + redCounter
+    blueCounter = img[r][c+1][1]*side + blueCounter
+    greenCounter = img[r][c+1][2]*side + greenCounter
 
-#Bottom middle
-redCounter = img[r][c+1][0]*side + redCounter
-blueCounter = img[r][c+1][1]*side + blueCounter
-greenCounter = img[r][c+1][2]*side + greenCounter
+    #Bottom right
+    redCounter = img[r+1][c+1][0]*diag + redCounter
+    blueCounter = img[r+1][c+1][1]*diag + blueCounter
+    greenCounter = img[r+1][c+1][2]*diag + greenCounter
 
-#Bottom right
-redCounter = img[r+1][c+1][0]*diag + redCounter
-blueCounter = img[r+1][c+1][1]*diag + blueCounter
-greenCounter = img[r+1][c+1][2]*diag + greenCounter
+    redAvg = redCounter
+    blueAvg = blueCounter
+    greenAvg = greenCounter
 
-redAvg = redCounter
-blueAvg = blueCounter
-greenAvg = greenCounter
-print("The average of the red pixels is:", redAvg)
-print("The average of the blue pixels is:", blueAvg)
-print("The average of the green pixels is:", greenAvg)
-#####################Average of quarter layer pixels (2x2)##########################
+    print("The average of the red pixels is:", redAvg)
+    print("The average of the blue pixels is:", blueAvg)
+    print("The average of the green pixels is:", greenAvg)
+    #####################Average of half layer pixels (2x3)##########################
+elif (c == colmax): # Right Column
+    #####################Average of half layer pixels (2x3)(Right Coloumn)##########################
+    redCounter = 0
+    blueCounter = 0
+    greenCounter = 0
+    middle = 0.33334
+    side = 0.16667
+    diag = 0.08334
+
+    #Middle
+    redCounter = img[r][c][0]*middle + redCounter
+    blueCounter = img[r][c][1]*middle + blueCounter
+    greenCounter = img[r][c][2]*middle + greenCounter
+
+    #Top left
+    redCounter = img[r-1][c-1][0]*diag + redCounter
+    blueCounter = img[r-1][c-1][1]*diag + blueCounter
+    greenCounter = img[r-1][c-1][2]*diag + greenCounter
+
+    #Top middle
+    redCounter = img[r][c-1][0]*side + redCounter
+    blueCounter = img[r][c-1][1]*side + blueCounter
+    greenCounter = img[r][c-1][2]*side + greenCounter
+
+    #Bottom middle
+    redCounter = img[r][c+1][0]*side + redCounter
+    blueCounter = img[r][c+1][1]*side + blueCounter
+    greenCounter = img[r][c+1][2]*side + greenCounter
+
+    #Bottom left
+    redCounter = img[r-1][c+1][0]*diag + redCounter
+    blueCounter = img[r-1][c+1][1]*diag + blueCounter
+    greenCounter = img[r-1][c+1][2]*diag + greenCounter
+
+    redAvg = redCounter
+    blueAvg = blueCounter
+    greenAvg = greenCounter
+
+    print("The average of the red pixels is:", redAvg)
+    print("The average of the blue pixels is:", blueAvg)
+    print("The average of the green pixels is:", greenAvg)
+    #####################Average of half layer pixels (2x3)##########################
+else: # Middle Pixels
+    #####################Average of full layer pixels (3x3)(Middle Pixels)##########################
+    redCounter = 0
+    blueCounter = 0
+    greenCounter = 0
+    middle = 0.25
+    side = 0.125
+    diag = 0.0625
+
+    #Top left
+    redCounter = img[r-1][c-1][0]*diag + redCounter
+    blueCounter = img[r-1][c-1][1]*diag + blueCounter
+    greenCounter = img[r-1][c-1][2]*diag + greenCounter
+
+    #Top middle
+    redCounter = img[r][c-1][0]*side + redCounter
+    blueCounter = img[r][c-1][1]*side + blueCounter
+    greenCounter = img[r][c-1][2]*side + greenCounter
+
+    #Top right
+    redCounter = img[r+1][c+1][0]*diag + redCounter
+    blueCounter = img[r+1][c+1][1]*diag + blueCounter
+    greenCounter = img[r+1][c+1][2]*diag + greenCounter
+
+    #Left
+    redCounter = img[r-1][c][0]*side + redCounter
+    blueCounter = img[r-1][c][1]*side + blueCounter
+    greenCounter = img[r-1][c][2]*side + greenCounter
+
+    #Middle
+    redCounter = img[r][c][0]*middle + redCounter
+    blueCounter = img[r][c][1]*middle + blueCounter
+    greenCounter = img[r][c][2]*middle + greenCounter
+
+    #Right
+    redCounter = img[r+1][c][0]*side + redCounter
+    blueCounter = img[r+1][c][1]*side + blueCounter
+    greenCounter = img[r+1][c][2]*side + greenCounter
+
+    #Bottom left
+    redCounter = img[r-1][c+1][0]*diag + redCounter
+    blueCounter = img[r-1][c+1][1]*diag + blueCounter
+    greenCounter = img[r-1][c+1][2]*diag + greenCounter
+
+    #Bottom middle
+    redCounter = img[r][c+1][0]*side + redCounter
+    blueCounter = img[r][c+1][1]*side + blueCounter
+    greenCounter = img[r][c+1][2]*side + greenCounter
+
+    #Bottom right
+    redCounter = img[r+1][c+1][0]*diag + redCounter
+    blueCounter = img[r+1][c+1][1]*diag + blueCounter
+    greenCounter = img[r+1][c+1][2]*diag + greenCounter
+
+    redAvg = redCounter
+    blueAvg = blueCounter
+    greenAvg = greenCounter
+
+    print("The average of the red pixels is:", redAvg)
+    print("The average of the blue pixels is:", blueAvg)
+    print("The average of the green pixels is:", greenAvg)
+    #####################Average of full layer pixels (3x3)##########################
+
+#Converts AxesImage into Image and saves the new image
+#converted = plt.imshow(img, interpolation='nearest')
+#plt.savefig('converted.png', bbox_inches='tight', pad_inches=0)
